67fd84368fb8024d45d6a6cb7b29698c383323cf
Allen Jay Desamparado <ajDesamparado13@gmail.com>
2020-08-10 16:14:54 +0800

    Update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import { VUE_INSTALLER as response} from './src/response'
-import { VUE_INSTALLER as login} from './src/login'
-import { VUE_INSTALLER as querifier} from './src/querifier'
-import { VUE_INSTALLER as mobile} from './src/mobile'
-import { VUE_INSTALLER as masks} from './src/masks'
-import { VUE_INSTALLER as formatter} from './src/formatter'
-import { VUE_INSTALLER as date} from './src/date'
+import { VUE_INSTALLER as response} from './src/utilities/response'
+import { VUE_INSTALLER as login} from './src/utilities/login'
+import { VUE_INSTALLER as querifier} from './src/utilities/querifier'
+import { VUE_INSTALLER as mobile} from './src/utilities/mobile'
+import { VUE_INSTALLER as masks} from './src/utilities/masks'
+import { VUE_INSTALLER as formatter} from './src/utilities/formatter'
+import { VUE_INSTALLER as date} from './src/utilities/date'
 
 const HelperUtility = {
     response,
