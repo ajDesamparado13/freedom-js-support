1ebc16b4ccd409cf7224b5dfd9fa0c19e10ae671
Allen Jay Desamparado <ajDesamparado13@gmail.com>
2020-10-27 19:54:24 +0800

    UPDATE

--- src/utilities/arr.js
+++ src/utilities/arr.js
@@ -1,11 +1,17 @@
 const Arr = {
     getObject(_config, defaultValue = {}, tap = null) {
-        let config = typeof _config === 'object' ? _config : defaultValue;
+        let config = this.isObject(_config) ? _config : defaultValue;
         return typeof tap === 'function' ? tap(config) : config
     },
     getProperty(_config, key, defaultValue, tap = null) {
         let property = (this.getObject(_config))[key] || defaultValue
         return typeof tap === 'function' ? tap(property) : property
+    },
+    isObject(_config){
+        return typeof _config === 'object'  && !Array.isArray(_config);
+    },
+    hasProperty(_config,key){
+        return this.isObject(_config) && _config.hasOwnProperty(key);
     }
 }
 
